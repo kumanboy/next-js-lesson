e68c33ddff4b2cf19f8a1fd6615e37deab04f433
kumanboy <husandavronov13@gmail.com>
2024-01-20 11:40:47 +0500

    backend management updated

--- src/app/api/users/[id]/route.tsx
+++ src/app/api/users/[id]/route.tsx
@@ -1,9 +1,50 @@
 import {NextResponse} from "next/server";
 import {users} from "@/app/utils/db";
+import fs from "fs";
 
 
-export async function  GET(_: any, res: { params: PromiseLike<{ id: any; }> | { id: any; }; }) {
+// Login
+export async function POST(req: {
+	json: () => PromiseLike<{ name: any; email: any; password: any; }> | { name: any; email: any; password: any; };
+}, res: { params: PromiseLike<{ id: any; }> | { id: any; }; }) {
+	let {name, email, password} = await req.json();
 	const {id} = await res.params;
-	const user = users.filter((u) => u.id == id);
-	return NextResponse.json({ user }, { status: 200 });
+
+	const {
+		name: userName,
+		email: userEmail,
+		password: userPassword,
+	} = users.find((u) => u.id == id);
+
+	if (userName === name && userEmail === email && userPassword === password) {
+		return NextResponse.json({result: "Login Successful"}, {status: 200});
+	} else if (!name || !email || !password) {
+		return NextResponse.json({result: "All fields are required"}, {status: 400});
+	} else {
+		return NextResponse.json({result: "Invalid Credentials"}, {status: 401});
+	}
 }
+// Delete user
+export async function DELETE(req: any, res: { params: PromiseLike<{ id: any; }> | { id: any; }; }){
+	const {id} = await res.params
+// 	userni idisi bo'yicha topish va o'chirish
+
+	const userIndex = users.findIndex((user) => user.id == id)
+
+	if (userIndex == -1) {
+		return NextResponse.json({result: "User not found"}, {status: 404})
+	}
+	// userni db.jsdan o'chirish
+	users.splice(userIndex, 1)
+
+	const updatedUsersArray = users
+	const updatedData = JSON.stringify(updatedUsersArray, null, 2)
+
+	fs.writeFileSync("./src/app/utils/db.js",
+		`export const users = ${updatedData}`,
+		"utf-8"
+	)
+
+	return NextResponse.json({result: "User deleted successfully"}, {status: 200})
+}
+
--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -1,8 +1,79 @@
 import {NextResponse} from "next/server";
 import {users} from "@/app/utils/db";
+import fs from "fs";
+
+//Create User
+
+export async function POST(req: {
+	json: () => PromiseLike<{ id: any; name: any; age: any; email: any; password: any; }> | {
+		id: any;
+		name: any;
+		age: any;
+		email: any;
+		password: any;
+	};
+}, res: any) {
+	let {id, name, age, email, password} = await req.json();
+
+	// Hamma malumotlar to'gri ekanligni tekshirish
+	if (!id || !name || !age || !email || !password) {
+		return NextResponse.json({result: "All fields are required"}, {status: 400});
+	} else {
+		// 	Yangi userni arrayga qo'shish
+		users.push({id, name, age, email, password});
+		// 	Updated data
+		const updatedUsersArray = users;
+		// 	Update qilingan userni JSON stringga o'zgartirish
+		const updatedData = JSON.stringify(updatedUsersArray, null, 2)
+
+		// 	db.js filega user malumotlarini qoshish
+		// fs haqida so'rang
+		fs.writeFileSync("./src/app/utils/db.js",
+			`export const users = ${updatedData}`,
+			"utf-8"
+		);
+
+		return NextResponse.json({result: "User created successfully"}, {status: 200});
+	}
+
+}
+
+export async function PUT(req: {
+	json: () => PromiseLike<{ id: any; name: any; age: any; email: any; password: any; }> | {
+		id: any;
+		name: any;
+		age: any;
+		email: any;
+		password: any;
+	};
+}, res: any) {
+	let {id, name, age, email, password} = await req.json();
+
+// 	userni idsini topish
+
+	const userIndex = users.findIndex((user) => user.id == id);
+
+	if (userIndex == -1) {
+		return NextResponse.json({result: "User not found"}, {status: 404});
+	}
+	if (name){
+		users[userIndex].name = name
+	}
+	if (email){
+		users[userIndex].email = email
+	}
+	if (password){
+		users[userIndex].password = password
+	}
+	const updatedUsersArray = users;
+	// 	Update qilingan userni JSON stringga o'zgartirish
+	const updatedData = JSON.stringify(updatedUsersArray, null, 2)
 
 
-export function GET() {
-	const data = users;
-	return NextResponse.json({ data }, { status: 200 });
+	fs.writeFileSync("./src/app/utils/db.js",
+		`export const users = ${updatedData}`,
+		"utf-8"
+	);
+
+	return NextResponse.json({result: "User updated successfully"}, {status: 200});
 }
